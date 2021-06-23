1fea177bc261fe8e9bfd7bc7c21a0c2898c3e289
Eshan <eshan.nalajala@gmail.com>
2021-06-23 15:46:36 -0500

    fixed preloader

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//setTimeout(function () {
-    //$('#loader').hide(500);
-    //$("#content").css("display", "inline");
-//}, 2500);
+setTimeout(function () {
+    $('#loader').hide(500);
+    $("#content").css("display", "inline");
+}, 2500);
 
